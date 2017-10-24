8fa66e5a36117da004af51049024f563d548b55e
Greg Meyer <greg.meyer@datadoghq.com>
2017-10-24 16:40:49 -0400

    adds jq, adds some better setup functionality

--- lib/run-datadog.sh
+++ lib/run-datadog.sh
@@ -4,12 +4,40 @@
 
 DATADOG_DIR="/home/vcap/app/datadog"
 
+get_vcap_var() {
+  echo $VCAP_APPLICATION | $DATADOG_DIR/jq -r $@
+}
+
+get_tags() {
+  local app_id=$(get_vcap_var '.application_id')
+  local app_name=$(get_vcap_var '.application_name')
+  local instance_index=$(get_vcap_var '.instance_index')
+  local space_name=$(get_vcap_var '.space_name')
+
+  tags="application_id:$app_id,application_name:$app_name,instance_index:$instance_index,space_name:$space_name"
+  echo $tags
+}
+
+start_datadog() {
+  pushd $DATADOG_DIR
+    local app_id=$(get_vcap_var '.application_id')
+    local app_name=$(get_vcap_var '.application_name')
+    local instance_index=$(get_vcap_var '.instance_index')
+    local space_name=$(get_vcap_var '.space_name')
+    export DD_LOG_FILE=$DATADOG_DIR/dogstatsd.log
+    export DD_LOG_LEVEL='debug'
+    export DD_API_KEY=$DATADOG_API_KEY
+    export DD_DD_URL=https://app.datadoghq.com
+    export DD_TAGS="application_id:$app_id application_name:$app_name instance_index:$instance_index space_name:$space_name"
+    echo $DD_TAGS
+    ./dogstatsd start &
+  popd
+}
+
 if [ -z ${DATADOG_API_KEY+x} ]; then
   echo "Datadog API Key not set, not starting Datadog"
 else
   echo "starting datadog"
 
-  pushd $DATADOG_DIR
-    DD_LOG_FILE=$DATADOG_DIR/dogstatsd.log DD_API_KEY=$DATADOG_API_KEY DD_DD_URL=https://app.datadoghq.com ./dogstatsd start &
-  popd
+  start_datadog
 fi
