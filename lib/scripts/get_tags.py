4bd9531bd395d2529ff33bc0af87be10fe026129
Greg Meyer <greg.meyer@datadoghq.com>
2017-10-25 11:19:46 -0400

    fix application_name tag

--- lib/scripts/get_tags.py
+++ lib/scripts/get_tags.py
@@ -3,14 +3,17 @@ import json
 
 vcap_application = json.loads(os.environ['VCAP_APPLICATION'])
 
-vcap_variables = ["application_id", "application_name", "instance_index", "space_name"]
+vcap_variables = ["application_id", "name", "instance_index", "space_name"]
 
 tags = []
 
 for vcap_var_name in vcap_variables:
     vcap_var = vcap_application.get(vcap_var_name)
     if vcap_var:
-        tags.append("{0}:{1}".format(vcap_var_name, vcap_var))
+        key = vcap_var_name
+        if vcap_var_name == 'name':
+            key = 'application_name'
+        tags.append("{0}:{1}".format(key, vcap_var))
 
 uris = vcap_application.get('uris')
 if uris:
