1204e99728399fa379118635a4345088b0e25ea3
Greg Meyer <greg.meyer@datadoghq.com>
2017-10-25 14:19:31 -0400

    proper production stuff

--- lib/run-datadog.sh
+++ lib/run-datadog.sh
@@ -7,8 +7,7 @@ DATADOG_DIR="/home/vcap/app/datadog"
 start_datadog() {
   pushd $DATADOG_DIR
     export DD_LOG_FILE=$DATADOG_DIR/dogstatsd.log
-    export DD_LOG_LEVEL='debug'
-    export DD_API_KEY=$DATADOG_API_KEY
+    export DD_API_KEY
     export DD_DD_URL=https://app.datadoghq.com
     datadog_tags=$(python $DATADOG_DIR/scripts/get_tags.py)
     sed -i "s/# tags:.*/tags: $datadog_tags/" $DATADOG_DIR/dist/datadog.yaml
@@ -16,7 +15,7 @@ start_datadog() {
   popd
 }
 
-if [ -z ${DATADOG_API_KEY+x} ]; then
+if [ -z ${DD_API_KEY+x} ]; then
   echo "Datadog API Key not set, not starting Datadog"
 else
   echo "starting datadog"
