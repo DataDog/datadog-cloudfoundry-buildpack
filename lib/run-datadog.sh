536c7147b09b027ccd191678b54fb32e849589a0
Greg Meyer <greg.meyer@datadoghq.com>
2018-03-23 17:49:17 -0400

    updates everything

--- lib/run-datadog.sh
+++ lib/run-datadog.sh
@@ -9,6 +9,12 @@ start_datadog() {
     export DD_LOG_FILE=$DATADOG_DIR/dogstatsd.log
     export DD_API_KEY
     export DD_DD_URL=https://app.datadoghq.com
+    export DD_ENABLE_CHECKS="${DD_ENABLE_CHECKS:-true}"
+    export DOCKER_DD_AGENT=yes
+
+    if [ "$DD_ENABLE_CHECKS" = "true" ]; then
+      sed -i "s~# confd_path:.*~confd_path: $DATADOG_DIR/conf.d~" $DATADOG_DIR/dist/datadog.yaml
+    fi
 
     # The yaml file requires the tags to be an array,
     # the conf file requires them to be comma separated only
@@ -20,8 +26,17 @@ start_datadog() {
     sed -i "s~# log_file: TRACE_LOG_FILE~log_file: $DATADOG_DIR/trace.log~" $DATADOG_DIR/dist/datadog.conf
     # Override user set tags so the tags set in the yaml file are used instead
     export DD_TAGS=""
-    ./dogstatsd start --cfgpath $DATADOG_DIR/dist/datadog.yaml &
-    ./trace-agent -config $DATADOG_DIR/dist/datadog.conf &
+
+    # DSD requires its own config file
+    cp $DATADOG_DIR/dist/datadog.yaml $DATADOG_DIR/dist/dogstatsd.yaml
+    if [ -n "$RUN_AGENT" -a -f ./puppy ]; then
+      export DD_LOG_FILE=$DATADOG_DIR/agent.log
+      ./puppy start --cfgpath $DATADOG_DIR/dist/ &
+    else
+      export DD_LOG_FILE=$DATADOG_DIR/dogstatsd.log
+      ./dogstatsd start --cfgpath $DATADOG_DIR/dist/ &
+    fi
+    ./trace-agent -ddconfig $DATADOG_DIR/dist/datadog.conf &
   popd
 }
 
