de7a0d75415634f967a9a77bd917e762166b59d8
Greg Meyer <gregory.meyer@datadoghq.com>
2018-05-24 13:08:49 -0400

    release 6.2.0, fix proxies (#8)

--- lib/run-datadog.sh
+++ lib/run-datadog.sh
@@ -8,7 +8,7 @@ start_datadog() {
   pushd $DATADOG_DIR
     export DD_LOG_FILE=$DATADOG_DIR/dogstatsd.log
     export DD_API_KEY
-    export DD_DD_URL=https://app.datadoghq.com
+    export DD_DD_URL=${DD_DD_URL:-https://app.datadoghq.com}
     export DD_ENABLE_CHECKS="${DD_ENABLE_CHECKS:-true}"
     export DOCKER_DD_AGENT=yes
 
@@ -21,22 +21,44 @@ start_datadog() {
     # so they must be grabbed separately
     datadog_tags=$(python $DATADOG_DIR/scripts/get_tags.py)
     sed -i "s/# tags:.*/tags: $datadog_tags/" $DATADOG_DIR/dist/datadog.yaml
-    legacy_datadog_tags=$(LEGACY_TAGS_FORMAT=true python $DATADOG_DIR/scripts/get_tags.py)
-    sed -i "s/# tags:.*/tags: $legacy_datadog_tags/" $DATADOG_DIR/dist/datadog.conf
-    sed -i "s~# log_file: TRACE_LOG_FILE~log_file: $DATADOG_DIR/trace.log~" $DATADOG_DIR/dist/datadog.conf
+    sed -i "s~log_file: TRACE_LOG_FILE~log_file: $DATADOG_DIR/trace.log~" $DATADOG_DIR/dist/datadog.yaml
+    if [ -n "$DD_SKIP_SSL_VALIDATION" ]; then
+      sed -i "s~# skip_ssl_validation: no~skip_ssl_validation: yes~" $DATADOG_DIR/dist/datadog.yaml
+    fi
     # Override user set tags so the tags set in the yaml file are used instead
     export DD_TAGS=""
+
+    if [ -n "DD_HTTP_PROXY" ]; then
+      sed -i "s~# proxy:~proxy:~" $DATADOG_DIR/dist/datadog.yaml
+      sed -i "s~#   http: HTTP_PROXY~  http: $DD_HTTP_PROXY~" $DATADOG_DIR/dist/datadog.yaml
+    else
+      if [ -n "HTTP_PROXY" ]; then
+        sed -i "s~# proxy:~proxy:~" $DATADOG_DIR/dist/datadog.yaml
+        sed -i "s~#   http: HTTP_PROXY~  http: $HTTP_PROXY~" $DATADOG_DIR/dist/datadog.yaml
+      fi
+    fi
+    if [ -n "DD_HTTPS_PROXY" ]; then
+      sed -i "s~# proxy:~proxy:~" $DATADOG_DIR/dist/datadog.yaml
+      sed -i "s~#   https: HTTPS_PROXY~  https: $DD_HTTPS_PROXY~" $DATADOG_DIR/dist/datadog.yaml
+    else
+      if [ -n "DD_HTTPS_PROXY" ]; then
+        sed -i "s~# proxy:~proxy:~" $DATADOG_DIR/dist/datadog.yaml
+        sed -i "s~#   https: HTTPS_PROXY~  https: $HTTPS_PROXY~" $DATADOG_DIR/dist/datadog.yaml
+      fi
+    fi
 
     # DSD requires its own config file
     cp $DATADOG_DIR/dist/datadog.yaml $DATADOG_DIR/dist/dogstatsd.yaml
     if [ -n "$RUN_AGENT" -a -f ./puppy ]; then
       export DD_LOG_FILE=$DATADOG_DIR/agent.log
+      sed -i "s~log_file: AGENT_LOG_FILE~log_file: $DD_LOG_FILE~" $DATADOG_DIR/dist/datadog.yaml
       ./puppy start --cfgpath $DATADOG_DIR/dist/ &
     else
       export DD_LOG_FILE=$DATADOG_DIR/dogstatsd.log
+      sed -i "s~log_file: AGENT_LOG_FILE~log_file: $DD_LOG_FILE~" $DATADOG_DIR/dist/datadog.yaml
       ./dogstatsd start --cfgpath $DATADOG_DIR/dist/ &
     fi
-    ./trace-agent -ddconfig $DATADOG_DIR/dist/datadog.conf &
+    ./trace-agent --config $DATADOG_DIR/dist/datadog.yaml &
   popd
 }
 
