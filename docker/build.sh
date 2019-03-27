538003a2474706ccc43eb860df6cb7b3bdf46d85
Greg Meyer <greg.meyer@datadoghq.com>
2019-03-27 13:40:01 +0100

    uncomment

--- docker/build.sh
+++ docker/build.sh
@@ -7,10 +7,10 @@ ROOT_DIR="$DOCKER_DIR/.."
 # First, build the buildpack.
 # The build script is the easiest way to get the buildpack assets
 
-# if [ -z "$DO_NOT_BUILD" ]; then
-#   pushd $ROOT_DIR
-#     NO_ZIP=true REFRESH_ASSETS=true ./build
-#   popd
-# fi
+if [ -z "$DO_NOT_BUILD" ]; then
+  pushd $ROOT_DIR
+    NO_ZIP=true REFRESH_ASSETS=true ./build
+  popd
+fi
 
 docker build $ROOT_DIR -t docker_app_with_buildpack -f $DOCKER_DIR/Dockerfile
