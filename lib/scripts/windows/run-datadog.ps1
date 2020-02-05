1e8809f16e7c9070e96598d4fe2de53e3e9240e7
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2020-02-05 15:26:34 -0500

    Switch to zip

--- lib/scripts/windows/run-datadog.ps1
+++ lib/scripts/windows/run-datadog.ps1
@@ -5,7 +5,7 @@ echo "  log_file: $DATADOG_DIR\trace.log" | Out-File "$DATADOG_DIR\AppData\datad
 
 $Env:LOGS_CONFIG_DIR="$DATADOG_DIR\AppData\conf.d\logs.d"
 New-Item $Env:LOGS_CONFIG_DIR -ItemType "directory" -Force
-Start-Process "$DATADOG_DIR\datadog agent\embedded2\python.exe" -ArgumentList "$DATADOG_DIR\scripts\create_logs_config.py" -NoNewWindow -Wait
+# Start-Process "$DATADOG_DIR\datadog agent\embedded2\python.exe" -ArgumentList "$DATADOG_DIR\scripts\create_logs_config.py" -NoNewWindow -Wait
 
-Start-Process "$DATADOG_DIR\datadog agent\bin\agent.exe" -ArgumentList "run -c $DATADOG_DIR\AppData" -NoNewWindow
-Start-Process "$DATADOG_DIR\datadog agent\bin\agent\trace-agent.exe" -ArgumentList "--config $DATADOG_DIR\AppData\datadog.yaml" -NoNewWindow
+Start-Process "$DATADOG_DIR\bin\agent.exe" -ArgumentList "run -c $DATADOG_DIR\AppData" -NoNewWindow
+Start-Process "$DATADOG_DIR\bin\agent\trace-agent.exe" -ArgumentList "--config $DATADOG_DIR\AppData\datadog.yaml" -NoNewWindow
