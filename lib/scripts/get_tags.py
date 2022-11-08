5c681f98f1d00455d53f139173158a7e5b5435f4
NouemanKHAL <noueman.khal@gmail.com>
2022-11-08 15:56:10 +0100

    filter user_data json tag

--- lib/scripts/get_tags.py
+++ lib/scripts/get_tags.py
@@ -23,9 +23,12 @@ tags.append("container_id:{}".format(os.environ.get("CF_INSTANCE_GUID")))
 if len(sys.argv) > 1 and sys.argv[1] == 'node-agent-tags':
     # These are always comma separated - See https://github.com/DataDog/datadog-agent/blob/main/pkg/cloudfoundry/containertagger/container_tagger.go#L133
     node_agent_tags = os.environ.get('DD_NODE_AGENT_TAGS', None)
+
     if node_agent_tags is not None:
+        node_agent_tags = node_agent_tags.replace(",\"", ";\"")
         all_node_agent_tags = node_agent_tags.split(",")
-        tags = tags + all_node_agent_tags
+        tags = tags + [tag for tag in all_node_agent_tags if ";" not in tag]
+
 
 for vcap_var_name in vcap_variables:
     vcap_var = vcap_application.get(vcap_var_name)
@@ -58,7 +61,7 @@ if user_tags:
     except Exception as e:
         print("there was an issue parsing the tags in DD_TAGS: {}".format(e))
 
-tags = list(set(tags))
+tags = list(dict.fromkeys(tags))
 
 legacy_tags = os.environ.get('LEGACY_TAGS_FORMAT', False)
 if legacy_tags:
