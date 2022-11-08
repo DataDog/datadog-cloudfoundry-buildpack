3c6145a73b61306336ccc0dcc7210f6b35955360
NouemanKHAL <noueman.khal@gmail.com>
2022-11-08 18:43:56 +0100

    append to the log file to keep track of all agent code executions

--- lib/scripts/update_agent_config.sh
+++ lib/scripts/update_agent_config.sh
@@ -8,6 +8,6 @@ while ! [ -f /home/vcap/app/.datadog/run/agent.pid ] && [ $timeout -lt 120 ]; do
 done
 
 # for debugging purposes
-echo $DD_NODE_AGENT_TAGS > /home/vcap/app/.datadog/dd-node-agent-tags.log
+echo $DD_NODE_AGENT_TAGS >> /home/vcap/app/.datadog/dd-node-agent-tags.log
 
 /bin/bash /home/vcap/app/.datadog/scripts/update_agent_config_restart.sh
