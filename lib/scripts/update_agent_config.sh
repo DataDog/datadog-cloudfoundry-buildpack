87c8ffbaf7602fccba1473af47140044eaa20559
NouemanKHAL <noueman.khal@gmail.com>
2022-11-08 15:58:10 +0100

    increase sleep and improve logging

--- lib/scripts/update_agent_config.sh
+++ lib/scripts/update_agent_config.sh
@@ -1,10 +1,13 @@
-# wait for agent startup
+#!/bin/sh
 
+# wait for the buildpack scripts to finish
 timeout=0
 while ! [ -f /home/vcap/app/.datadog/run/agent.pid ] && [ $timeout -lt 120 ]; do
-    sleep 0.5
+    sleep 1
     timeout=$((timeout+1))
 done
 
+# for debugging purposes
+echo $DD_NODE_AGENT_TAGS > /home/vcap/app/.datadog/dd-node-agent-tags.log
 
 /bin/bash /home/vcap/app/.datadog/scripts/update_agent_config_restart.sh
