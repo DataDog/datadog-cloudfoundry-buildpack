9c20fd502ec5b8333948841c82ec7f9df98a51bd
Sarah Witt <sarah.witt@datadoghq.com>
2022-12-08 07:09:16 -0800

    Wait for agent startup before updating CAPI tags (#145)
    * Properly wait for agent startup
    
    * Wait for token as well
    
    * small refactor
    
    * Add back datadog dir when using as argument; harmonize compile and supply
    
    * Fix pid file name
    
    Co-authored-by: Noueman Khalikine <noueman.khalikine@datadoghq.com>

--- lib/scripts/update_agent_config.sh
+++ lib/scripts/update_agent_config.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
-# wait for the buildpack scripts to finish
-timeout=0
-while { ! [ "$(pgrep -f ./agent)" = "" ] && ! [ "$(pgrep -f ./dogstatsd)" = "" ]; } && [ $timeout -lt 120 ]; do
-    sleep 1
-    timeout=$((timeout+1))
-done
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2.0 License.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2022-Present Datadog, Inc.
 
+DATADOG_DIR="${DATADOG_DIR:-/home/vcap/app/.datadog}"
+DEBUG_FILE="${DATADOG_DIR}/update_agent_config_out.log"
+
+main() {
+    # wait for the buildpack scripts to finish
+    echo "Starting to wait for agent process to start"
+    timeout 120s "${DATADOG_DIR}/scripts/check_datadog.sh"
+
+    echo "$DD_NODE_AGENT_TAGS"
+
+    /bin/bash "${DATADOG_DIR}/scripts/update_agent_config_restart.sh"
+}
 # for debugging purposes
-echo $DD_NODE_AGENT_TAGS >> /home/vcap/app/.datadog/dd-node-agent-tags.log
-
-/bin/bash /home/vcap/app/.datadog/scripts/update_agent_config_restart.sh
+main "$@" >> "$DEBUG_FILE" 2>&1
