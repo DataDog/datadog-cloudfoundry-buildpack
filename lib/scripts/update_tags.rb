3089405f950a276ae27225daacced2dc8cf78038
Sarah Witt <sarah.witt@datadoghq.com>
2023-01-04 11:19:11 -0500

    Remove app_instance_guid from node agent tags.txt

--- lib/scripts/update_tags.rb
+++ lib/scripts/update_tags.rb
@@ -23,6 +23,7 @@ tags = []
 def sanitize(tags_env_var)
     tags_list = tags_env_var.gsub(",\"", ";\"").split(",")
     tags_list.keep_if { |element| !element.include?(";") }
+    tags_list.keep_if { |element| !element.include?("app_instance_guid") }
     tags_list = tags_list.map { |tag| tag.gsub(" ", "_") }
     return tags_list.uniq
 end
