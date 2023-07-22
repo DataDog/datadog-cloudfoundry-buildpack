89516d6d00640bf17fe50a0381982dcabb4e2da8
Noueman Khalikine <noueman.khalikine@datadoghq.com>
2023-07-22 13:22:42 +0200

    recompute DD_TAGS in test-endpoint.sh using ruby

--- lib/test-endpoint.sh
+++ lib/test-endpoint.sh
@@ -4,6 +4,11 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2017-Present Datadog, Inc.
 
+DATADOG_DIR="${DATADOG_DIR:-/home/vcap/app/.datadog}"
+
+# source updated PATH
+. "$DATADOG_DIR/.global_env"
+
 unset DD_LOGS_VALID_ENDPOINT
 DATADOG_DIR="${DATADOG_DIR:-/home/vcap/app/.datadog}"
 DD_EU_API_SITE="https://api.datadoghq.eu/api/"
@@ -81,7 +86,7 @@ if [ "${DD_LOGS_ENABLED}" = "true" ] && [ -n "${DD_LOGS_CONFIG_LOGS_DD_URL}" ] &
             \"title\": \"Log endpoint cannot be reached - Log collection not started\",
             \"text\": \"Could not establish a connection to ${DD_LOGS_CONFIG_LOGS_DD_URL} after 5 seconds. Log collection has not been started.\",
             \"priority\": \"normal\",
-            \"tags\": \"$DD_TAGS\",
+            \"tags\": $(ruby "${DATADOG_DIR}"/scripts/get_tags.rb),
             \"alert_type\": \"error\"
       }" "${DD_API_SITE}v1/events?api_key=${DD_API_KEY}"
   else
