187ab8da8056c0880d2dedf0015ede4f6e5abf8d
NouemanKHAL <noueman.khalikine@datadoghq.com>
2023-10-12 18:28:00 +0200

    Update the build script to use the VERSION file by default (#180)

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,16 +3,10 @@
 set -euxo pipefail
 
 main() {
-  if [ -z "${VERSION}" ]; then
-    echo "VERSION is required to build the buildpack"
-    exit 0
-  fi
-
+  VERSION=${VERSION:-$(cat VERSION)}
   srcDir=$(pwd)
   name="datadog-cloudfoundry-buildpack"
   zip="${name}-${VERSION}.zip"
-
-  echo "${VERSION}" > VERSION
 
   rm -f ${zip}
 
